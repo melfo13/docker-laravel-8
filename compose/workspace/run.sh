8d6836b029d0c44f2b08f50f9d5e91b62843dbbd
melfo13 <melfo13@gmail.com>
2022-04-15 13:45:17 -0600

    Make project compatible with php8

--- compose/workspace/run.sh
+++ compose/workspace/run.sh
@@ -11,13 +11,13 @@ php -v
 
 # docker-compose run --rm npm install 
 
-# docker-compose run --rm artisan clear:data
-# docker-compose run --rm artisan cache:clear 
-# docker-compose run --rm artisan view:clear 
-# docker-compose run --rm artisan route:clear 
-# docker-compose run --rm artisan clear-compiled 
-# docker-compose run --rm artisan config:cache
-# docker-compose run --rm artisan key:generate
-# docker-compose run --rm artisan storage:link
-# docker-compose run --rm artisan migrate --seed
-# docker-compose run --rm artisan passport:install
+docker-compose run --rm artisan clear:data
+docker-compose run --rm artisan cache:clear 
+docker-compose run --rm artisan view:clear 
+docker-compose run --rm artisan route:clear 
+docker-compose run --rm artisan clear-compiled 
+docker-compose run --rm artisan config:cache
+docker-compose run --rm artisan key:generate
+docker-compose run --rm artisan storage:link
+docker-compose run --rm artisan migrate --seed
+docker-compose run --rm artisan passport:install
